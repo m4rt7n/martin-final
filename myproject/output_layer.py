dd67835c7441b8cea7c4d1d7f249b650f66e351c
m4rt7n <martinvmgm@gmail.com>
2024-11-17 21:46:15 -0500

    changes

--- myproject/output_layer.py
+++ myproject/output_layer.py
@@ -1,7 +1,7 @@
 # Archivo: output_layer.py
 import json
 
-def generate_report(letter_soup, words, output_file="reporte_sopa_letras.json"):
+def generate_report(letter_soup, words):
     """
     Genera un reporte en formato JSON con las palabras encontradas y no encontradas en la sopa de letras.
     
@@ -9,6 +9,7 @@ def generate_report(letter_soup, words, output_file="reporte_sopa_letras.json"):
     :param words: Lista de palabras a buscar.
     :param output_file: Nombre del archivo JSON de salida.
     """
+    output_file="soup_report.json"
     from logic_layer import find_words  # Importar la función find_words desde la capa lógica
     
     # Encontrar las palabras en la sopa de letras utilizando la función find_words
@@ -26,7 +27,7 @@ if __name__ == "__main__":
     from input_layer import get_file_content  # Importar la función get_file_content desde la capa de entrada
     
     # Definir la ruta del archivo de entrada (modificar según la ubicación del archivo)
-    file_path = "sopa_letras.txt"  # Cambiar por la ruta del archivo de entrada
+    file_path = "letter_soup.txt"  # Cambiar por la ruta del archivo de entrada
     
     # Obtener la sopa de letras y las palabras a buscar desde el archivo de entrada
     letter_soup, words = get_file_content(file_path)
